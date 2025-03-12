647eae501292ffbd3f11be42ca6dfaf5c6c6c12a
akai <mistah.akai@gmail.com>
2025-03-12 15:37:51 -0400

    updated transforms

--- plugin/transform.py
+++ plugin/transform.py
@@ -16,21 +16,27 @@ def cmd(author, namespace, send_message):
 
     message = namespace.strip()
     msg = message.split()
-    commands = [".transform"]
+    commands = [".transform", ".plan9", ".henshin"]
     user_list = ["Akai◆giko//JRnk", 
                  "meow◆meow/DrxXk", 
                  "Archduke◆cRwJk8JEBs", 
                  "gyudon_addict◆hawaiiZtQ6"]
     
-    if msg[0] in commands:
-        if msg[0] == ".transform":
-            if author in user_list:
+    
+    if author in user_list:
+        if msg[0] in commands:
+            if msg[0] == ".transform":
                 random_binary = randompull(filename)
                 ascii_output = bin_to_ascii(random_binary)
                 send_message(ascii_output)
-            elif author not in user_list:
-                # DISREGARD MSG
-                not_authorized = (
+            elif msg[0] == ".plan9":
+                send_message("#gnot")
+            elif msg[0] == ".henshin":
+                send_message("#henshin")
+    else:
+        if author not in user_list:
+            # DISREGARD MSG
+            not_authorized = (
     "01010000 01001100 01000101 01000001 01010011 01000101 "
     "00100000 "
     "01000100 01001001 01010011 01010010 01000101 01000111 01000001 01010010 01000100 "
@@ -42,7 +48,8 @@ def cmd(author, namespace, send_message):
     "01010011 01010101 01000011 01001011 "
     "00100000 "
     "01000011 01001111 01000011 01001011 01010011"
-)
+    
+            )
 
-                ascii_error = bin_to_ascii(not_authorized)
-                send_message(f"#spy {author} shouts to the world, \"{ascii_error}!\"")
+            ascii_error = bin_to_ascii(not_authorized)
+            send_message(f"#spy {author} shouts to the world, \"{ascii_error}!\"")
